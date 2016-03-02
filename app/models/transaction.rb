9fd711f6303bfffef20222d17c1191a8636323d3
Daniel Buompane <danielbuompane@gmail.com>
2016-03-02 17:30:05 -0500

    Get total balance of wallet

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ActiveRecord::Base
+
+  def self.balance
+    self.all.reduce(0.0) {|sum, i| sum + i.amount}
+  end
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "get wallet balance" do
+    assert_equal 20.00, Transaction.balance
+  end
 end
