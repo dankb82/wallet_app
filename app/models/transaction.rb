1fbedbdea0b4058460c710970a01faae773345e6
Daniel Buompane <danielbuompane@gmail.com>
2016-03-02 21:39:55 -0500

    Refactor self.balance method. #Shipit :shipit:

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,10 +1,18 @@
 class Transaction < ActiveRecord::Base
 
   def self.balance
-    self.all.reduce(0.0) {|sum, i| sum + i.amount}
+    credit = 0.0
+    debit = 0.0
+    self.all.each do |i|
+      credit += i.amount if i.credit_or_debit == "credit"
+      debit += i.amount if i.credit_or_debit == "debit"
+    end
+    credit - debit
   end
 
   def self.total_transactions
     self.count
   end
+
+  # def current_month_
 end
